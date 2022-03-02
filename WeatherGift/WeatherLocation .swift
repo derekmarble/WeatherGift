6c65668b638051ddbc1060308e409bcb6e7dcf6e
Derek Marble <dmarble20@gmail.com>
2022-03-02 15:10:25 -0500

    6.2: WeatherLocations List Table View

---
+++ WeatherGift/WeatherLocation .swift
@@ -0,0 +1,14 @@
+//
+//  WeatherLocation .swift
+//  WeatherGift
+//
+//  Created by Derek Marble on 3/2/22.
+//
+
+import Foundation
+
+struct WeatherLocation  {
+    var name: String
+    var latitude: Double
+    var longitube: Double
+}
