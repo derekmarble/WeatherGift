c8ab82235d8af2d6956b77d6fae784ec0c62e258
Derek Marble <dmarble20@gmail.com>
2022-03-14 15:24:37 -0400

    6.11: OpenWeather Update - Parsing JSON results using Codable

--- WeatherGift/WeatherLocation .swift
+++ WeatherGift/WeatherLocation .swift
@@ -18,36 +18,5 @@ class WeatherLocation: Codable  {
         self.longitude = longitude
     }
     
-    func getData() {
-        let urlString = "https://api.openweathermap.org/data/2.5/onecall?lat=\(latitude)&lon=\(longitude)&exclude=minutely&units=imperial&appid=\(APIkeys.openWeatherKey)"
-        
-        print("We are accessing the URl \(urlString)")
-        
-        //create a URL
-              guard let url = URL(string: urlString) else {
-                  print("ERROR: could create a URL from \(urlString)")
-                  return
-              }
-              
-         //create session
-        let session = URLSession.shared
-              
-        //get data with data.task method
-        let task = session.dataTask(with: url) { data, response, error in
-            if let error = error {
-                print("ERROR: \(error.localizedDescription)")
-            }
-        
-        
-        //deal with the data
-            do {
-                let json = try JSONSerialization.jsonObject(with: data!, options: [])
-                print(json)
-            } catch {
-                print("JSON ERROR: \(error.localizedDescription)")
-                
-            }
-        }
-        task.resume()
-    }
+   
 }
