36325e9a623aa39d81d889d410a1004dca88d647
Derek Marble <dmarble20@gmail.com>
2022-03-26 15:59:52 -0400

    6.13 OpenWeather - Working with Dates, Times, TimeZones

--- WeatherGift/LocationDetailViewController.swift
+++ WeatherGift/LocationDetailViewController.swift
@@ -7,6 +7,14 @@
 
 import UIKit
 
+private let dateFormatter: DateFormatter = {
+    print("I just created a date formatter!")
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = "EEEE, MMM d, h:mm aaa"
+    return dateFormatter
+}()
+
+
 class LocationDetailViewController: UIViewController {
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var placeLabel: UILabel!
@@ -37,7 +45,9 @@ class LocationDetailViewController: UIViewController {
         pageControl.currentPage = locationIndex
         weatherDetail.getData {
             DispatchQueue.main.async {
-                self.dateLabel.text = self.weatherDetail.timezone
+                dateFormatter.timeZone = TimeZone(identifier: self.weatherDetail.timezone)
+                let usableDate = Date(timeIntervalSince1970: self.weatherDetail.currentTime)
+                self.dateLabel.text = dateFormatter.string(from: usableDate)
                 self.placeLabel.text = self.weatherDetail.name
                 self.temperatureLabel.text = "\(self.weatherDetail.temperature)°"
                 self.summaryLabel.text = self.weatherDetail.summary
