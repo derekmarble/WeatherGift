b396922103944f28e70396f4858b9338f5676a6b
Derek Marble <dmarble20@gmail.com>
2022-03-27 18:05:32 -0400

    6.19: Getting the device location with CoreLocation

--- WeatherGift/LocationDetailViewController.swift
+++ WeatherGift/LocationDetailViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import CoreLocation
 
 private let dateFormatter: DateFormatter = {
     let dateFormatter = DateFormatter()
@@ -26,6 +27,7 @@ class LocationDetailViewController: UIViewController {
     
     var weatherDetail: WeatherDetail!
     var locationIndex = 0
+    var locationManager: CLLocationManager!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -42,6 +44,10 @@ class LocationDetailViewController: UIViewController {
         
         collectionView.delegate = self
         collectionView.dataSource = self
+        
+        if locationIndex == 0 {
+            getLocation()
+        }
         
         updateUserInterface()
     }
@@ -134,4 +140,63 @@ extension LocationDetailViewController: UICollectionViewDelegate, UICollectionVi
         return weatherDetail.hourlyWeatherData.count
     }
     
+}
+extension LocationDetailViewController: CLLocationManagerDelegate {
+    func getLocation() {
+        //Creating a CLLocationManager will automatically check for authorization
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        print("Checking authentication status.")
+        handleAuthorizationStatus(status: status)
+    }
+    
+    func handleAuthorizationStatus(status: CLAuthorizationStatus) {
+        switch status {
+            
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+        case .restricted:
+            self.oneButtonAlert(title: "Location services denied.", message: "It may be that parental controls are restricting location use in this app.")
+        case .denied:
+            //TODO: handle alert with ability to change
+            break
+        case .authorizedAlways, .authorizedWhenInUse:
+            locationManager.requestLocation()
+        @unknown default:
+            print("DEVELOPER ALERT: Unknown case of status in handleAuthenticationStatus \(status)")
+        }
+    }
+  
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let currentLocation = locations.last ?? CLLocation()
+        print("Current location is \(currentLocation.coordinate.latitude), \(currentLocation.coordinate.longitude)")
+        let geocoder = CLGeocoder()
+        geocoder.reverseGeocodeLocation(currentLocation) { placemarks, error in
+            var locationName = ""
+            if placemarks != nil {
+                //get the first placemark
+                let placemark = placemarks?.last
+                //assign placemark to locationName
+                locationName = placemark?.name ?? "Parts Unknown"
+            } else {
+                print("Error retrieving place.")
+                locationName = "Could not find location"
+            }
+            print("locationName = \(locationName)")
+            
+            //update weatherLocations[0] with the current location so it can be used in updateUserInterface. getLocation only called when locationIndex == 0
+            let pageViewController = UIApplication.shared.windows.first!.rootViewController as! PageViewController
+            pageViewController.weatherLocations[self.locationIndex].latitude = currentLocation.coordinate.latitude
+            pageViewController.weatherLocations[self.locationIndex].longitude = currentLocation.coordinate.longitude
+            pageViewController.weatherLocations[self.locationIndex].name = locationName
+            self.updateUserInterface()
+        }
+    }
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("ERROR. \(error.localizedDescription). Failed to get device location.")
+    }
 }
