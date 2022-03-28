0a04a94d61177424bf469edf0495c640690d6215
Derek Marble <dmarble20@gmail.com>
2022-03-27 20:02:41 -0400

    6.22: Finished WeatherGift: LaunchScreen, About Screen, and App icon

---
+++ WeatherGift/AboutViewController.swift
@@ -0,0 +1,20 @@
+//
+//  AboutViewController.swift
+//  WeatherGift
+//
+//  Created by Derek Marble on 3/27/22.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+    @IBAction func doneButtonPressed(_ sender: UIBarButtonItem) {
+        dismiss(animated: true, completion: nil)
+    }
+}
